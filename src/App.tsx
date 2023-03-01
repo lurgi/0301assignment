4700bb33262a262a1c1b0fea09f768bca67de62f
lurgi <wahoo9040@naver.com>
2023-03-01 22:21:06 +0900

    0301

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
-import ToDoList from './ToDoList';
+import { Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -68,8 +69,10 @@ a {
 function App() {
   return (
     <>
-      <GlobalStyle/>
-      <ToDoList/>
+      <GlobalStyle />
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<ToDoList />} />
+      </Routes>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from 'styled-components';
-import App from './App';
-import { theme } from './theme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
+    <BrowserRouter>
+      <RecoilRoot>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
-    </RecoilRoot>
+      </RecoilRoot>
+    </BrowserRouter>
   </React.StrictMode>
 );
